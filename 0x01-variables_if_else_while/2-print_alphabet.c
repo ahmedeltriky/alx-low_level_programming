12baa003973bd29ee75a0d1328c3dbffed655f5b
Ahmed Elsayed <ahmedeltriky@gmail.com>
2023-03-17 22:19:25 +0200

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 
-  
+/**
+ * main - Entry
+ * Return: value 0
+ */
 int main()
 {
-  // set both the starting and ending alphabet
-  char start = 'a';
-  char end = 'z' ;
+	char start = 'a';
+	char end = 'z' ;
+  	char ch = start;
 
-  // initialize ch with the starting alphabet as a
-  char ch = start;
-
-  // print out characters from the initial value of ch til before e
-  while(ch != end)
-  {
-     putchar(ch);
-     ch++;
-  }
-  putchar('z');
+	 while(ch != end)
+{
+    	 putchar(ch);
+    	 ch++;
+}
+ 	 putchar('z');
 
   return 0;
 }
