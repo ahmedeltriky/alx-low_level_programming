99b1f9f9e247dd79516310c5a74e79fc25455b63
Ahmed Elsayed <37491034+ahmedeltriky@users.noreply.github.com>
2023-03-21 14:43:24 +0200

    all tasks

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Function
+ * Description: Fibonacci sequence up to 4,000,000
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long int prev, curr, sum, next;
+
+	prev = 1;
+	curr = 2;
+	sum = 0;
+
+	while (curr <= 4000000)
+	{
+		if (curr % 2 == 0)
+		{
+			sum += curr;
+		}
+
+		next = prev + curr;
+
+		prev = curr;
+
+		curr = next;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
