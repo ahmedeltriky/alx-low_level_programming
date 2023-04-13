937631593ba52cb37750e0e954ac9c61e7b01d23
Ahmed Elsayed <37491034+ahmedeltriky@users.noreply.github.com>
2023-04-13 21:47:21 +0200

    re task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,122 +1,126 @@
+#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
-/* First I am Proud of what i did here */
+#include <ctype.h>
 
 /**
- * checkNum - this function check if the strings passed as an arguments
- * if all the strings are digits the everything is fine
- * else it print Error and exit with the code of 98
- * @str1: the first stirng
- * @str2: the second string
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: nothing just checking
-*/
-int checkNum(char *str1, char *str2)
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
 {
-	int len1 = strlen(str1) - 1, len2 = strlen(str2) - 1, i = 0;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (i = 0; i < len1; i++)
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
 	{
-		if (!isdigit(str1[i]))
+		printf("0\n");
+		exit(0);
+	}
+}
+
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-	}
 
-	for (i = 0; i < len2; i++)
-	{
-		if (!isdigit(str2[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-
-	return (0);
+	return (ln);
 }
 
 /**
- * initMemory - this function makes memory for the result
- * @size: is the size of length of two strings
- * Return: a pointer the the first elements of the array
-*/
-char *initMemory(int size)
-{
-	int i;
-	char *array;
-
-	array = malloc((size + 2) * sizeof(char));
-	if (array == NULL)
-		return (NULL);
-
-
-	for (i = 0; i < size + 2; i++)
-	{
-		array[i] = '0';
-	}
-
-	return (array);
-}
-/**
- * multiply - here where the magic happens
- * this function takes num1 and num2 which are the stirng passed as argv
- * it loop trough every character and multiply it with the other one
- * and push the result of all into the array that i initMemory above
- * @num1: is the first argument
- * @num2: is the second argument
- * @array: is an array of zeros i initalize it above
- * Return: nothing
-*/
-char  *multiply(char *num1, char *num2, char *array)
-{
-	int p, i, j, len1 = strlen(num1), len2 = strlen(num2);
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			p = (num1[i] - '0') * (num2[j] - '0') + (array[i + j + 1] - '0');
-			array[i + j + 1] = p % 10 + '0';
-			array[i + j] += p / 10;
-		}
-	}
-
-
-	for (i = 0; i < len1 + len2; i++)
-	{
-		if (array[i] != '0')
-			return (&array[i]);
-	}
-	return ("0");
-}
-
-/**
- * main - this where the program call all the functions
- * @argc: length of arguments passed
- * @argv: an array of arguments passed
- * Return: 0
-*/
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
 int main(int argc, char *argv[])
 {
-	int len1, len2;
-	char *array;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		printf("Error\n");
-		exit(98);
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-	len1 = strlen(argv[1]) - 1;
-	len2 = strlen(argv[2]) - 1;
-
-	checkNum(argv[1], argv[2]);
-	array = initMemory(len1 + len2);
-	printf("%s\n", multiply(argv[1], argv[2], array));
-	free(array);
+	printf("%s\n", nout);
 	return (0);
-
 }
-
